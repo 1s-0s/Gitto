8137788fc452c5f2563c83cf9b1e47a887fe0797
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2021-03-12 13:59:45 +0530

    Added module

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const chalk = require("chalk");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
-
+const auth = require("./routes/auth");
 require("./passport/githubConfig");
+
 
 const app = express();
 
@@ -20,17 +21,19 @@ app.use(session({
 }))
 app.use(passport.initialize());
 app.use(passport.session())
+app.use("/", auth);
+//app.use("/auth/github/dashboard", auth);
 
 //! app.use(express.static)
 
-app.get("/auth/github", passport.authenticate("github", { scope: ["profile"] }), (req, res) => {
-    console.log(chalk.red("Wasnt ment to run"));
-})
+// app.get("/auth/github", passport.authenticate("github", { scope: ["profile"] }), (req, res) => {
+//     console.log(chalk.red("Wasnt ment to run"));
+// })
 
-app.get("/auth/github/dashboard", passport.authenticate("github", { failureRedirect: "http://localhost:3000/" }), (req, res) => {
-    console.log(chalk.green("successfully callback"));
-    res.redirect("http://localhost:3000/#/dashboard");
-})
+// app.get("/auth/github/dashboard", passport.authenticate("github", { failureRedirect: "http://localhost:3000/" }), (req, res) => {
+//     console.log(chalk.green("successfully callback"));
+//     res.redirect("http://localhost:3000/#/dashboard");
+// })
 
 app.listen(5000, () => {
     console.log(chalk.blue("Server is running boiss"));
