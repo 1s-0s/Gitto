8800357375a21488cb1976fd58b919133e9c605d
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2021-03-13 14:58:55 +0530

    Updated user schema and add .env for credentials

--- models/User.js
+++ models/User.js
@@ -2,6 +2,30 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    githubId : String 
-})
-module.exports =  mongoose.model("users",userSchema);
+    githubId: String,
+    name: String,
+    email: String,
+    likes: Number,
+    follower: Number,
+    following: Number,
+    gist: String,
+    bio: String,
+    repositry: Number,
+    blog: String,
+    location: String,
+    friends: [{
+        githubId: String,
+        name: String,
+        email: String,
+    }],
+    image: [{
+        type: String
+    }],
+    technology: [{
+        type: String
+    }],
+},
+    {
+        timestamps: true
+    })
+module.exports = mongoose.model("users", userSchema);
--- passport/githubConfig.js
+++ passport/githubConfig.js
@@ -2,42 +2,56 @@ const passport = require("passport");
 const chalk = require("chalk");
 const GitHubStrategy = require("passport-github2").Strategy;
 const mongoose = require('mongoose');
+//database
 const User = mongoose.model("users");
+//dotenv configuration
+require("dotenv").config();
 
-const GITHUB_CLIENT_ID = "7cd2a780b218a17d6194";
-const GITHUB_CLIENT_SECRET = "83501798296561ca0147d1634900af29e845a493";
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
 passport.serializeUser((user, done) => {
-    done(null, user);
+  done(null, user);
 })
 passport.deserializeUser((obj, done) => {
-    done(null, obj);
+  done(null, obj);
 })
 
 passport.use(
-    new GitHubStrategy(
-      {
-        clientID: GITHUB_CLIENT_ID,
-        clientSecret: GITHUB_CLIENT_SECRET,
-        callbackURL: "http://localhost:5000/auth/github/dashboard",
-      },
-      async (accessToken, refreshToken, profile, done) => {
-        try{
-          console.log(profile);
-          const currUser = await User.findOne({ githubId: profile.id });
-          if (currUser) {
-            return done(null, profile);
-          } else {
-            const newUser = new User({ githubId: profile.id });
-            const savedUser = await newUser.save();
-            done(null, savedUser);
-          }
-        }
-        catch(err){
-            console.log(chalk.red(err));
+  new GitHubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:5000/auth/github/dashboard",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        console.log(profile);
+        const currUser = await User.findOne({ githubId: profile.id });
+        if (currUser) {
+          return done(null, profile);
+        } else {
+          const newUser = new User({
+            githubId: profile.id,
+            name: profile.name,
+            email: profile.email,
+            follower: profile.followers,
+            following: profile.following,
+            gist: profile.public_gists,
+            bio: profile.bio,
+            repositry: profile.public_repos,
+            blog: profile.blog,
+            location: profile.location
+          });
+          const savedUser = await newUser.save();
+          done(null, savedUser);
         }
       }
-    )
-  );
+      catch (err) {
+        console.log(chalk.red(err));
+      }
+    }
+  )
+);
 
 
