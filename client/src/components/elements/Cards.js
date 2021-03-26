52a0805f14381b6b5dc4e7f9cadf5d5e7b07b7c2
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2021-03-26 17:21:23 +0530

    Added dislike/like

--- client/src/components/elements/Cards.js
+++ client/src/components/elements/Cards.js
@@ -11,10 +11,10 @@ import {
   // ReactIcon
 } from "../styles/CardStyle";
 // Action Creator
-import {reloadComponentAction } from "../../action/index";
+import { reloadComponentAction } from "../../action/index";
 //React-Redux
 import { bindActionCreators } from "redux";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 //React Syntax Highlighter
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { qtcreatorDark, atelierSavannaLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
@@ -26,12 +26,12 @@ class Card extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      like: false,
-     
+      like: null,
+      dislike: null
     }
-    
+
   }
-  code=`
+  code = `
   function createStyleObject(classNames, style) {
     return classNames.reduce((styleObject, className) => {
       return {...styleObject, ...style[className]};
@@ -41,16 +41,27 @@ class Card extends React.Component {
   function createClassNameString(classNames) {
       return classNames.join(' ');
   }`
-  
-  
+
+
   toggleLike = async () => {
-    await this.setState({ like: this.state.like === true ? false : true });
-    console.log("like: ", this.state.like);
+
+    if (this.state.like === null || this.state.like === false) {
+      await this.setState({ like: true });
+    }
+    // else if (this.state.like === true) {
+    //   await this.setState({ like: null });
+    // }
+    console.log("like setting to like: ", this.state.like);
   };
 
   toggleDisLike = async () => {
-    await this.setState({ like: this.state.like === true ? false : true });
-    console.log("dislike: ", this.state.like);
+    if (this.state.like === null || this.state.like === true) {
+      await this.setState({ like: false });
+    }
+    // else if (this.state.like === false) {
+    //   await this.setState({ like: null });
+    // }
+    console.log("dislike setting to like state: ", this.state.like);
   };
   //? adding card user to users friendlist
   addToFriends(friend) {
@@ -61,7 +72,8 @@ class Card extends React.Component {
       data: { "friend": friend }
     })
       .then((response) => {
-        console.log("friend added successfully", response);
+        // console.log("friend added successfully", response);
+        alert(`${friend.name} has been added successfully!`);
         this.props.reloadComponentAction();
       })
       .catch((err) => {
@@ -70,40 +82,40 @@ class Card extends React.Component {
   }
   render() {
     // console.log(this.props.key);
-    const currTheme= this.props.theme.name;
-    console.log("currTheme is : ",currTheme)
+    const currTheme = this.props.theme.name;
+    // console.log("currTheme is : ", currTheme)
     return (
       <CardSegment>
-        
-        <Gist showLineNumbers  style={currTheme==='light' ? atelierSavannaLight : qtcreatorDark}>
+
+        <Gist showLineNumbers style={currTheme === 'light' ? atelierSavannaLight : qtcreatorDark}>
           {this.props.gist}
         </Gist>
-          {/* <Gist id={this.props.gistid} /> */}
-        
+        {/* <Gist id={this.props.gistid} /> */}
+
         <CardDetails>
           <CardHeader>
             {this.props.name}
-            <CardButton circular color="teal" size="mini" floated="right" icon="arrow down" onClick={this.toggleDisLike}></CardButton>
-            <CardButton circular color="teal" size="mini" floated="right" icon="arrow up" onClick={this.toggleLike}></CardButton>
+            <CardButton circular color={this.state.like ? "teal" : "red"} size="mini" floated="right" icon="arrow down" onClick={this.toggleDisLike}></CardButton>
+            <CardButton circular color={this.state.like ? "red" : "teal"} size="mini" floated="right" icon="arrow up" onClick={this.toggleLike}></CardButton>
             <CardButton circular color="teal" size="mini" floated="right" icon="plus" onClick={() => this.addToFriends(this.props.friend)}></CardButton>
           </CardHeader>
           <SubHeader>
-            <LangLabel style={{color:"green"}}>MongoDB</LangLabel>
-            <LangLabel style={{color:"red"}}>MongoDB</LangLabel>
-            <LangLabel style={{color:"blue"}}>MongoDB</LangLabel>
+            <LangLabel style={{ color: "green" }}>MongoDB</LangLabel>
+            <LangLabel style={{ color: "red" }}>MongoDB</LangLabel>
+            <LangLabel style={{ color: "blue" }}>MongoDB</LangLabel>
           </SubHeader>
         </CardDetails>
       </CardSegment>
     );
   }
 }
-const mapDispatchToProps = (dispatch)=>{
+const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({ reloadComponentAction }, dispatch);
 }
 const mapStateToProps = (state) => {
-  console.log("user card: ",state.auth.user);
+  // console.log("user card: ", state.auth.user);
   return {
     user: state.auth.user
   }
 }
-export default connect(mapStateToProps,mapDispatchToProps)(Card);
+export default connect(mapStateToProps, mapDispatchToProps)(Card);
