f2bc4dca3813fa4ff5026d181f7ede274b565ebd
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2021-03-30 17:23:14 +0530

    Updated logn UI

--- client/src/components/routes/Login.js
+++ client/src/components/routes/Login.js
@@ -1,10 +1,30 @@
 import React from "react";
-import { Anchor, Div, Icon } from "../styles/LoginStyle";
+import Typewriter from "typewriter-effect";
+import { Anchor, Div, Icon, Header, SubHeader } from "../styles/LoginStyle";
 class Login extends React.Component {
 
     render() {
         return (
             <Div>
+                <Header>
+                    <Typewriter
+                        options={{
+                            strings: ["GIT-TO"],
+                            autoStart: true,
+                            loop: true
+                        }}
+                        onInit={
+                            (typewriter) => {
+                                typewriter
+                                    .pauseFor(1000)
+                                    .deleteAll()
+                                    .start();
+                            }}
+                    />
+                </Header>
+                <SubHeader>
+                    GIT to find your pair-programmer
+                </SubHeader>
                 <Anchor className="ui github circular button massive" href="http://localhost:5000/auth/github/">
                     <Icon className="github icon "></Icon>
                     Github
--- client/src/components/styles/LoginStyle.js
+++ client/src/components/styles/LoginStyle.js
@@ -5,15 +5,27 @@ export const Anchor = styled.a`
     top: 50% !important;
     left: 50% !important;
     transform: translate(-50%, -50%) !important;
-    width: 10em !important;
+    width: 8em !important;
     background-color: white !important;
     color: #06090F !important;
     
 `;
-export const Icon=styled.i`
+export const Icon = styled.i`
     font-size: 30px  !important;
     color: #06090F !important;
 
+`;
+export const Header = styled.div`
+    padding-top: 25vh;
+    font-size: 4em;
+    text-align: center;
+    color: white;
+`;
+export const SubHeader = styled.p`
+    padding-top: 5vh;
+    font-size: 1.5em;
+    text-align: center;
+    color: white;
 `;
 export const Div = styled.div`
     background-image:linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(${loginbg});
