cbca3312662b4a5018217fea4b7a4c99d4f44b39
Ritik <ritikpr307@gmail.com>
2021-05-25 18:36:37 +0530

    deploy heroku production-2

--- passport/githubConfig.js
+++ passport/githubConfig.js
@@ -28,7 +28,7 @@ passport.use(
     {
       clientID: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: "https://git-to.herokuapp.com/auth/github/dashboard",
+      callbackURL: "/auth/github/dashboard",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
